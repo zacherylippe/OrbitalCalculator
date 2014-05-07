3565a34a18e6f0236abe27377ecfaee6a1f34521
zacherylippe <th3ranger@gmail.com>
2014-05-07 17:29:18 -0400

    More work

--- orbital calc/orbital calc/Program.cs
+++ orbital calc/orbital calc/Program.cs
@@ -17,9 +17,27 @@ namespace orbital_calc
             double massKerbin = 5.2915793*(Math.Pow(10,22));
             const double kerbinRadius = 600000;
             const double earthRadius = 6371000;
+            double desiredOrbit = 0;
+            char planet = 'A';
+            double orbit = 0;
+            double radius = 0;
             double result = 0;
 
-            result = (gravitationalConstant * massEarth * .000000149745433);
+            Console.Write("Which planet would you like to calculate an orbit for?\r\n");
+            Console.Write("K for Kerbin E for Earth.");
+            planet = Convert.ToChar(Console.Read());
+            if (planet == 'K')
+            {
+                radius = kerbinRadius;
+            }
+            else
+            {
+                radius = earthRadius;
+            }
+            Console.Write("What is the desired orbit in kilometers?");
+            orbit = Console.ReadDouble();
+            orbit = orbit * 1000;
+            result = (gravitationalConstant * massEarth * (1/orbit));
             result = Math.Sqrt(result);
             Console.Write(result);
         }
