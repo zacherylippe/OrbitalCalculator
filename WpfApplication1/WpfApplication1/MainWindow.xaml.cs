b1e2c4ec9464abbf779b847920c9ca617b6787cb
zacherylippe <th3ranger@gmail.com>
2014-05-09 12:59:52 -0400

    WPF stage

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
+        {
+            const double gravityAndDragEst = 1.20512821; //wild estimate of gravitiational and air drag combined
+            const double gravitationalConstant = .0000000000667384;
+            double massEarth = 5.97219 * (Math.Pow(10, 24)); //I'm shocked this works
+            double massKerbin = 5.2915793 * (Math.Pow(10, 22));
+            const double kerbinRadius = 600000;
+            const double earthRadius = 6371000;
+            double desiredOrbit = 0;
+            char planet = 'A';
+            double orbit = 0;
+            double radius = 0;
+            double result = 0;
+
+            Console.Write("Which planet would you like to calculate an orbit for?\r\n");
+            Console.Write("K for Kerbin E for Earth.");
+            planet = Convert.ToChar(Console.Read());
+            if (planet == 'K')
+            {
+                radius = kerbinRadius;
+            }
+            else
+            {
+                radius = earthRadius;
+            }
+            Console.Write("What is the desired orbit in kilometers?");
+            orbit = Console.ReadDouble();
+            orbit = orbit * 1000;
+            result = (gravitationalConstant * massEarth * (1 / orbit));
+            result = Math.Sqrt(result);
+            Console.Write(result);
+        }
+    }
+}
